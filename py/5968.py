4d6e3ef4c8122dee6c6f04c0c2d7240d4ba49a5a
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2025-03-11 14:33:05 +0800

    Update 5968.py

--- py/5968.py
+++ py/5968.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # masked_phone = phonenum[:3] + '****' + phonenum[-4:]
 phonenum = '13007665968'
 masked_phone = phonenum
-log_filename = "5968.log"
+log_filename = "app.log"
 
 def aliyun(tab, browser):
     
